fd25e53bb28970aeecc4a01ea8386cf5a285145e
ParkJinSuk <ParkJinSuk@users.noreply.github.com>
2020-02-06 08:35:41 +0900

    .

---
+++ Arduino/robot/robot.ino
@@ -0,0 +1,21 @@
+
+/*  프로그램 내용 : 이 로봇은 5개의 IR센서를 사용하여
+ *  라인을 트랙킹하며 길을 찾아 서빙을 합니다.
+ *  
+ *  프로그램 작성 날짜 : 2020년 2월 5일
+ *  
+ *  [수정날짜0205] - 핀번호 추가
+ */
+
+#define 
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
