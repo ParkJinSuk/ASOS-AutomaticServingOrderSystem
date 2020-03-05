57aff99ae94e180dee8b5067b6a9e1549d8165c4
wodnjs3574 <60778827+wodnjs3574@users.noreply.github.com>
2020-03-06 00:28:10 +0900

    Feedback algorithm

--- Arduino/feedback_control/feedback_control.ino
+++ Arduino/feedback_control/feedback_control.ino
@@ -1,174 +1,112 @@
-// Demo for getting individual unified sensor data from the MPU6050
-#include <Adafruit_MPU6050.h>
+// encoder pin
+const int encoderPinA = 2;
+const int encoderPinB = 3;
 
-Adafruit_MPU6050 mpu;
-Adafruit_Sensor *mpu_temp, *mpu_accel, *mpu_gyro;
+long encoderPos = 0;
+int angle = 0;
+int time1 = 0;
+int value = 0;
 
+double v;
 
-double a, sum=0, v=0, init_a=0, tmp, tmp_a=0;
-int i=1;
+float Kp = 4.2227;
+float Ki = 2.29743;
+float Kd = -0.03976;
+float Ke = 0.084;
 
-float Kp = 1.82946985924925;
-float Ki = 0.070383050819117;
-float Kd = -2.23583790055285;
+float input_v=35;
 
-float input_v=0.5;
-float measured_v, tmp_v=0;
-
-double t=1/100;
+double t=0.01;
 double PControl, IControl, DControl, PIDControl;
 double error, errorPrevious=input_v;
 int pwm_in;
 
 void setup(void) {
+  pinMode(encoderPinA, INPUT_PULLUP);
+  attachInterrupt(0, doEncoderA, CHANGE);
+ 
+  pinMode(encoderPinB, INPUT_PULLUP);
+  attachInterrupt(1, doEncoderB, CHANGE);
+ 
   Serial.begin(115200);
-
-  Serial.println("Adafruit MPU6050 test!");
-
-  if (!mpu.begin()) {
-    Serial.println("Failed to find MPU6050 chip");
-    while (1) {
-      delay(10);
-    }
-  }
   
-  Serial.println("MPU6050 Found!");
+  pinMode(9, OUTPUT);       // Motor B 방향설정1
+  pinMode(10, OUTPUT);       // Motor B 방향설정2
 
-  mpu_accel = mpu.getAccelerometerSensor();
-  mpu_accel->printSensorDetails();
-  
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
 
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
+  Serial.println("Starting..");
+  motorA(0, 0);
+  delay(2000);
+  time1 = millis();
 
-  for(int i=0; i<10; i++){
-      sensors_event_t accel;
-      mpu_accel->getEvent(&accel);
-      tmp=accel.acceleration.z;
-      init_a+=tmp;
-  }
-  
-  init_a=init_a/10;
-
-  Serial.print("init_a: "); Serial.println(init_a);
-  
-  delay(5000);
+  value = 207;
+  Serial.print("start speed : ");
+  Serial.println(value);
+  motorA(1, value);
 }
 
 void loop() {
-  //  /* Get a new normalized sensor event */
-  sensors_event_t accel; 
-  mpu_accel->getEvent(&accel);
+  if( millis()%10 == 0){
+    pos2ang();
+    Serial.println(angle);
+    v=3.15*angle;    
+    error=(input_v-v)*Ke;
 
-  /* Display the results (acceleration is measured in m/s^2) */
-  /*
-  Serial.print("Accel X: ");
-  Serial.print(accel.acceleration.x);
-  Serial.print(" Y: ");
-  Serial.print(accel.acceleration.y);
-  Serial.print(" Z: ");
-  Serial.print(accel.acceleration.z);
-  */
+    PControl=Kp*error;
+    IControl=Ki*error*t;
+    DControl=Kd*(error-errorPrevious)/t;
+
+    PIDControl=PControl+IControl+DControl;
+
+    pwm_in=255/6*PIDControl;
   
-  
-/*
-  
-  analogWrite(5, 150);
-  analogWrite(6, 0);
-
-  analogWrite(2, 0);
-  analogWrite(3, 150);
-  */
-
-  /*
-  a=accel.acceleration.x*accel.acceleration.x+accel.acceleration.y*accel.acceleration.y+accel.acceleration.z*accel.acceleration.z;
-  a=sqrt(a);
-  */
-  a=-(accel.acceleration.z-init_a);
-
-  a=(tmp_a*i+a)/(i+1);  //칼만필터 on
-  
-  tmp_a=a;
-  
-  v=a*i* 0.01; 
-
-  measured_v=v*18.62607;   //칼만 필터가 없을 때
-  
-
-  
-  error=input_v-v;
-
-  PControl=Kp*error;
-  IControl=Ki*error*t;
-  DControl=Kd*(error-errorPrevious)/t;
-
-  PIDControl=PControl+IControl+DControl;
-  /*
-  if(PIDControl<=0.33 && PIDControl>=0)
-    pwm_in=100;
-  else if(PIDControl>0.33 && PIDControl<=0.58)
-    pwm_in=5.8962*PIDControl*100-90.2547;
-  else if(PIDControl<0)
-    pwm_in=0;
-  else
-    pwm_in=255;
-
-  
-  analogWrite(5, pwm_in);
-  analogWrite(6, 0);
-
-  analogWrite(2, 0);
-  analogWrite(3, pwm_in);  
-*/  
-
-  analogWrite(5, 0);
-  analogWrite(6, 200);
-
-  analogWrite(2, 200);
-  analogWrite(3, 0);  
-  
-  errorPrevious=error;
-
-
-
-  Serial.print("  a: "); Serial.print(a);
-  Serial.print(" measured v: "); Serial.print(measured_v); Serial.print(" pwm_in:"); Serial.print(pwm_in); 
-  Serial.print(" PIDControl: "); Serial.print(PIDControl);
-  Serial.print(" error: "); Serial.print(error);
-  Serial.print(" v: "); Serial.println(v); 
-  
-  i++;
-  
-  
-  if(i>1000){
-    analogWrite(5, 0);
-    analogWrite(6, 0);
-
-    analogWrite(2, 0);
-    analogWrite(3, 0);
-    Serial.println("여기가 끝");
-    while(1);
+    motorA(1, pwm_in);
+    errorPrevious=error;
   }
-  
+}
 
-  
-  delay(t*1000);
 
-  /*   serial plotter friendly format
-  Serial.print(temp.temperature);
-  Serial.print(",");
+void doEncoderA(){ // 빨녹일 때
+//  if(digitalRead(encoderPinA)==digitalRead(encoderPinB)) // 같으면
+//    encoderPos++; // 정회전
+//  else // 다르면
+//    encoderPos--; // 역회전
+  encoderPos += (digitalRead(encoderPinA)==digitalRead(encoderPinB))?1:-1;
+  //Serial.print("A   ");
+  //Serial.println(encoderPos);
+}
 
-  Serial.print(accel.acceleration.x);
-  Serial.print(","); Serial.print(accel.acceleration.y);
-  Serial.print(","); Serial.print(accel.acceleration.z);
-  Serial.print(",");
+void doEncoderB(){ // 보파일 때
+//  if(digitalRead(encoderPinA)==digitalRead(encoderPinB)) // 같으면
+//    encoderPos--; // 역회전
+//  else // 다르면
+//    encoderPos++; // 정회전
+  encoderPos += (digitalRead(encoderPinA)==digitalRead(encoderPinB))?-1:1;
+  //Serial.print("B   ");
+  //Serial.println(encoderPos);
+}
 
-  Serial.print(gyro.gyro.x);
-  Serial.print(","); Serial.print(gyro.gyro.y);
-  Serial.print(","); Serial.print(gyro.gyro.z);
-  Serial.println();
-  delay(10);
-  */
+
+void motorA(int dir, int _speed)
+{
+  if (dir == 1)
+  {
+    analogWrite(9, 0);
+    analogWrite(10, _speed);
+  }
+  else if (dir == 2)
+  {
+    analogWrite(9, _speed);
+    analogWrite(10, 0);
+  }
+  else
+  {
+    analogWrite(9, 0);
+    analogWrite(10, 0);
+  }
+}
+
+void pos2ang()
+{
+  angle = encoderPos/(341.2*4) * 360    /360*2*3.141592;
 }
